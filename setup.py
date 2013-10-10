235635236c0197e5f00b4b1b2adb035ffdb68850
Benoit Landry <landry@mit.edu>
2013-10-09 23:18:09 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,6 +7,5 @@ setup(name='Garmin Extractor',
       description='Extracts FIT files from ANT-FS based sport watches such as Garmin Forerunner 60, 405CX, 310XT, 610 and 910XT.',
       author='Gustav Tiger',
       url='https://github.com/Tigge/Garmin-Forerunner-610-Extractor',
-      packages=['garminextractor'],
-      requires=['pyusb==1.0.0a3'],
+      install_requires=['pyusb==1.0.0a3'],
      )
